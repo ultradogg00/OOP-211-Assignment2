d1459239355db1f819111bf02fece99e1d2af4e4
Chelsea Yang <chelsea.yang8@gmail.com>
2024-03-10 23:57:17 -0600

    handed in version

---
+++ Traveless-HandIn/Models/Flight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveless.Models
+{
+    public class Flight
+    {
+        // open and read file hear
+        // dtrubets of of all types of file 
+        // time cost day etc
+        public string FlightCode {  get; set; }
+        public string AirlineName { get; set; } 
+        public string AirportDeptName { get; set; }
+        public string AirportArivName { get; set; }
+        public string DepartDate { get; set; }
+        public string DepartTime { get; set; }
+        public string Duration { get; set; }
+        public double Cost { get; set; }
+
+        public Flight(string Code,string airName,string portDeptName, string portArivName ,string date,string departTime,string duration, double cost) 
+        { 
+            FlightCode = Code;
+            AirlineName = airName;
+            AirportDeptName = portDeptName;
+            AirportArivName = portArivName;
+            DepartDate = date;
+            DepartTime = departTime;
+            Duration = duration;
+            Cost = cost;
+        }
+        public string getFlightInfo()
+        {
+            return $"{FlightCode}, {AirlineName}, {AirportDeptName},{AirportArivName}, {DepartDate} ,{DepartTime} ,{Duration}, {Cost}";
+        }
+
+    }
+}
---
+++ Traveless/bin/Debug/net8.0-windows10.0.19041.0/win10-x64/AppX/Components/Pages/Reservation.razor.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Traveless.Models;
+
+namespace Traveless.Components.Pages;
+
+public partial class Reservation : ComponentBase
+{
+	string resCode { get; set; } = string.Empty;
+	string resAirline { get; set; } = string.Empty;
+	string resDepartDay { get; set; } = string.Empty;
+	string resDepartureTime { get; set; } = string.Empty;
+	string resName { get; set; } = string.Empty;
+	string resCitizenship { get; set; } = string.Empty;
+    bool resIsActive { get; set; } = false; 
+    public List<ReservationModel> allReservations = new List<ReservationModel>();
+    public List<ReservationModel> foundReservations = new List<ReservationModel>();
+
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        // read the file
+        //wwwroot/reservations.csv
+        string resDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot/Data");
+
+        string reservationsFilePath = Path.Combine(resDirectory, "reservations.csv");
+
+        try
+        {
+            // Read the contents of the file
+            string[] reservationsContent = File.ReadAllLines(reservationsFilePath);
+
+
+            foreach (string line in reservationsContent)
+            {
+                string[] data = line.Trim().Split(',');
+
+                ReservationModel reservationModel= new ReservationModel(data[0], data[1], data[2], data[3], data[4], data[5]);
+                allReservations.Add(reservationModel);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that may occur
+            Console.WriteLine($"Error reading the file: {ex.Message}");
+        }
+    }
+
+    // looks for existing reservations:
+    public async void findReservation(string code, string airline, string name)
+    {
+        foundReservations.Clear();
+        foreach (ReservationModel reservation in allReservations) 
+        {
+            if (string.Equals(code, reservation.Name, StringComparison.OrdinalIgnoreCase) && string.Equals(airline, reservation.Airline, StringComparison.OrdinalIgnoreCase) && string.Equals(name, reservation.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                reservation.isActive = true;
+                foundReservations.Add(reservation);
+                LoadSelectdReservation(reservation);
+                return;
+            }
+        }
+        await Application.Current.MainPage.DisplayAlert("Invalid Fields", "Please enter the valid reservation codes, airline, and name.", "OK");
+    }
+
+    // found reservation:
+    public void LoadSelectdReservation(ReservationModel selReservation)
+    {
+        resCode = selReservation.Code.Remove(2,1);
+        resAirline = selReservation.Airline;
+        resDepartDay = selReservation.Day;
+        resDepartureTime = selReservation.Time;
+        resName = selReservation.Name;
+        resCitizenship = selReservation.Citizenship;
+        resIsActive = selReservation.isActive;
+    }
+
+    // update reservation if there are changes in the fields:
+    public void updateReservation(string newName, string newCitizenship, bool newsIsActive)
+    {
+        foreach (ReservationModel reservation in allReservations)
+        {
+            if (reservation.Name != newName || reservation.Citizenship != newCitizenship || reservation.isActive != newsIsActive)
+            {
+                reservation.Name = newName;
+                reservation.Citizenship = newCitizenship;
+                reservation.isActive = newsIsActive;
+            } 
+        }
+    }
+
+}
